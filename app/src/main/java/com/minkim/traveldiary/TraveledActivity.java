9cc9053b983d7967fd9e2ea01ffb24a74f521398
roseanna <roseanna.chu@bc.edu>
2016-04-28 13:01:43 -0400

    Internet
    Added internet permission..
    Need to figure out the api key

--- app/src/main/java/com/minkim/traveldiary/TraveledActivity.java
+++ app/src/main/java/com/minkim/traveldiary/TraveledActivity.java
@@ -1,12 +1,25 @@
 package com.minkim.traveldiary;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class TraveledActivity extends AppCompatActivity {
 
@@ -26,6 +39,43 @@ public class TraveledActivity extends AppCompatActivity {
         delete  = (Button) findViewById(R.id.delete);
         edit    = (Button) findViewById(R.id.edit);
         view    = (Button) findViewById(R.id.view);
+        CityScrape cs = new CityScrape();
+        String testCity = "Boston";
+        cs.execute(testCity);
+    }
+    private class CityScrape extends AsyncTask<String, String, String>{
+        private ProgressDialog progressDialog = new ProgressDialog(TraveledActivity.this);
+        StringBuilder result = new StringBuilder();
 
+        protected void onPreExecute() {
+            progressDialog.setMessage("Downloading your data...");
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            String apiKey = "AIzaSyCMWujTZzTbnI7g-TzQmEUiizMSncHPbkM";
+            String jsonUrl = "https://maps.googleapis.com/maps/api/place/autocomplete/xml?input="+ params[0]
+                    + "&types=(cities)&key=" + apiKey;
+            try {
+                URL url = new URL(jsonUrl);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+                String line;
+                while ((line = reader.readLine()) != null){
+                    result.append(line);
+                    Log.i("result", line);
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
     }
 }

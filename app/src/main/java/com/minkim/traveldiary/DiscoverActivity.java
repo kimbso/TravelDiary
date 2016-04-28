369a4ca9b891d8f4e0e7d3953699a98524e06202
kimbso <kimbso@bc.edu>
2016-04-28 13:27:53 -0400

    new activities, png, xml

--- app/src/main/java/com/minkim/traveldiary/DiscoverActivity.java
+++ app/src/main/java/com/minkim/traveldiary/DiscoverActivity.java
@@ -1,13 +1,38 @@
 package com.minkim.traveldiary;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.*;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.*;
+
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class DiscoverActivity extends AppCompatActivity {
+
+    EditText city, country;
+    TextView info;
+    Button search, weather, plane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover);
+
+        final Intent intent = getIntent();
+
+        city    = (EditText) findViewById(R.id.city);
+        country = (EditText) findViewById(R.id.country);
+        info    = (TextView) findViewById(R.id.info);
+        search  = (Button)   findViewById(R.id.search);
+        weather = (Button)   findViewById(R.id.weather);
+        plane   = (Button)   findViewById(R.id.plane);
     }
 }
